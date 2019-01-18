4a38c2463ab2bf2e37d48142f4eff072918611f8
Pablo-Es <szyposz@gmail.com>
2019-01-18 22:25:49 +0100

    generate squares

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-let lifes = document.querySelector('js-time-score__life-number');
-let points = document.querySelector('js-time-score__points-nmb');
-let time = document.querySelector('js-time-score__time');
+let lifes = document.querySelector('.js-time-score__life-number');
+let points = document.querySelector('.js-time-score__points-nmb');
+let time = document.querySelector('.js-time-score__time');
+const table = document.querySelector('.js-table');
 
 
-
-const squareNumberInRow = 5;
+const squareNumberInRow = 6;
 const setSquare = (squareNumberInRow) => {
-    let tr = '';
-    let output = '';
+let output ='';
 for (let i = 0; i < squareNumberInRow; i++) {
-    output += '<tr></tr>';
+
+    let td = '';
     for(let j = 0; j < squareNumberInRow; j++) {
-        output+= '<td></td>';
+        td+= `<td data-tdId="td_${i}${j}"></td>`;
     }
-    console.log(output);
+    output += `<tr>${td}</tr>`;
+
+
 }
+    table.innerHTML = output;
+
 };
 setSquare(squareNumberInRow);
