7b4669ed80b15b07654ba50d757bf03771a02809
Pablo-Es <szyposz@gmail.com>
2019-01-19 22:25:43 +0100

    create function for adding active class

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-let lifes = document.querySelector('.js-time-score__life-number');
-let points = document.querySelector('.js-time-score__points-nmb');
-let time = document.querySelector('.js-time-score__time');
+const lifes = document.querySelector('.js-time-score__life-number');
+const points = document.querySelector('.js-time-score__points-nmb');
+const time = document.querySelector('.js-time-score__time');
 const table = document.querySelector('.js-table');
+let td = document.getElementsByTagName('td');
 
+let tdArray;
 
-const squareNumberInRow = 6;
+const squareNumberInRow = 5;
+const lifesNumber = 3;
+const initialTime = 60;
+let scoreCounter  = 0;
+
 const setSquare = (squareNumberInRow) => {
 let output ='';
 for (let i = 0; i < squareNumberInRow; i++) {
@@ -17,7 +23,29 @@ for (let i = 0; i < squareNumberInRow; i++) {
 
 
 }
+
     table.innerHTML = output;
+
 
 };
 setSquare(squareNumberInRow);
+const shuffleSquare = () => {
+
+    tdArray = [...td];
+    return tdArray.sort(() => Math.random() - 0.5);
+};
+const addActiveClass = () => {
+  let randomIndex = Math.floor(Math.random()* td.length + 1);
+    tdArray = [...td];
+    tdArray = tdArray.map((item, idx) => {
+       if(idx === randomIndex) {
+           item.classList.add("active");
+       }
+    });
+   // console.log(tdArray.ind;exOf(randomIndex))
+  // tdArray.indexOf(randomIndex).classList.add("active");
+};
+addActiveClass();
+console.log('tdArray=',tdArray);
+
+
